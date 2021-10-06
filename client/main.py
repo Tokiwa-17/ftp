c072e138abdebc1aea6a86d02c09304656f9b19a
ylf <2872444509@qq.com>
2021-10-06 23:26:48 +0800

    LOGGING IN (USER/PASS COMMANDS)

--- client/main.py
+++ client/main.py
@@ -1,19 +1,28 @@
 import socket
 import sys
 
-def sock_client_data():
-    while True:
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect(('192.168.44.131', 1234))  #服务器和客户端在不同的系统或不同的主机下时使用的ip和端口，首先要查看服务器所在的系统网卡的ip
-            # s.connect(('127.0.0.1', 6666))  #服务器和客户端都在一个系统下时使用的ip和端口
-        except socket.error as msg:
-            print(msg)
-            print(sys.exit(1))
-        data = input("input data:")   #输入要传输的数据
-        s.send(data.encode())  #将要传输的数据编码发送，如果是字符数据就必须要编码发送
-        s.close()
+def login():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('192.168.44.131', 1234))
+
+    except socket.error as msg:
+        print(msg)
+        print(sys.exit(1))
+    # 1. respond with an initial message
+    s.recv(1024)
+    # 2. client to send a “USER anonymous” command.
+    str = 'USER anonymous\0'
+    s.send(str.encode())
+    # 3. ask for an email address as a password
+    s.recv(1024)
+    email = input("Please input an email address for logging in: ") + '\0'
+    s.send(email.encode())
+    # 4. rcv greetings
+    s.recv(1024).decode("utf-8", "ignore")
+
+    s.close()
 
 
 if __name__ == '__main__':
-    sock_client_data()
+    login()
