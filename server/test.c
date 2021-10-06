06d3aecd42b504f235fefdb9d0ba53c207c87b00
Tokiwa-17 <2872444509@qq.com>
2021-10-06 20:06:44 +0800

    完成主机和虚拟机的通信

--- server/test.c
+++ server/test.c
@@ -5,28 +5,34 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-int main(){
-    //创建套接字
-    int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    //将套接字和IP、端口绑定
+
+void login() {
+    int serve_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     struct sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));  //每个字节都用0填充
     serv_addr.sin_family = AF_INET;  //使用IPv4地址
     serv_addr.sin_addr.s_addr = inet_addr("192.168.44.131");  //具体的IP地址
-    serv_addr.sin_port = htons(1234);  //端口
-    bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-    //进入监听状态，等待用户发起请求
-    listen(serv_sock, 20);
-    //接收客户端请求
-    struct sockaddr_in clnt_addr;
-    socklen_t clnt_addr_size = sizeof(clnt_addr);
-    int clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
-    //向客户端发送数据
-    char str[] = "Hello World!";
-    write(clnt_sock, str, sizeof(str));
-   
-    //关闭套接字
-    close(clnt_sock);
-    close(serv_sock);
+    serv_addr.sin_port = htons(1234); 
+    bind(serve_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    listen(serve_sock, 10);
+    printf("Wait for Connection..................\n");
+    while(1) {
+        struct sockaddr_in clnt_addr;
+        socklen_t clnt_addr_size = sizeof(clnt_addr);
+        int clnt_sock = accept(serve_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
+        char buf[1024];
+        int msg = recv(clnt_sock, buf, sizeof(buf) - 1, 0);
+        printf("Msg of rcv: %d\n", msg);
+        printf("Message from client: %s\n", buf);
+        
+        close(clnt_sock);
+        //close(serve_sock);
+    }
+    close(serve_sock);
+    return;
+}
+
+int main(){
+    login();
     return 0;
 }
