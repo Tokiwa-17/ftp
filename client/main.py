f8cf7c49c48b5f6c07d58cd11187db8d6100cc4e
ylf <2872444509@qq.com>
2021-10-08 12:59:15 +0800

    SYST client初步完成

--- client/main.py
+++ client/main.py
@@ -38,14 +38,25 @@ def pasv():
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(('192.168.44.133', 1234))
-    except sockete.error as msg:
+    except socket.error as msg:
         print(msg)
         print(sys.exit(1))
     s.recv(50)
     print("PASV success!")
 
+def syst():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(('192.168.44.133', 1234))
+    except socket.error as msg:
+        print(msg)
+        print(sys.exit(1))
+    s.recv(50)
+    print("SYST success!")
+
 if __name__ == '__main__':
     #login()
     #port()
-    pasv()
+    #pasv()
+    syst()
 
