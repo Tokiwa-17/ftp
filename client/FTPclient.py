5f94b0d846db126672433685ad1617a89a451b1a
ylf <2872444509@qq.com>
2021-10-21 15:51:31 +0800

    client upload & download

--- client/FTPclient.py
+++ client/FTPclient.py
@@ -2,7 +2,7 @@ import socket
 import sys
 import os
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QMainWindow, QMessageBox, QTreeWidgetItem, QTreeWidget, QDirModel, QFileSystemModel
+from PyQt5.QtWidgets import QMainWindow, QMessageBox, QTreeWidgetItem, QTreeWidget, QDirModel, QFileSystemModel, QInputDialog, QLabel
 from login import Ui_login
 from PyQt5.QtCore import QDir
 from cmd import Ui_cmd
@@ -47,13 +47,13 @@ class FTPClient(QMainWindow):
         self.cmd = Cmd()
         self.window = Window()
         self.client.mode = 'PASV'
-        self.cmd.cmd_window.PORT_radioButton.clicked.connect(self.port_mode)
-        self.cmd.cmd_window.PASV_radioButton.clicked.connect(self.pasv_mode)
-        self.cmd.cmd_window.mkdir_pushButton.clicked.connect(self.mkdir)
-        self.cmd.cmd_window.rmdir_pushButton.clicked.connect(self.rmdir)
+        #self.cmd.cmd_window.PORT_radioButton.clicked.connect(self.port_mode)
+        #self.cmd.cmd_window.PASV_radioButton.clicked.connect(self.pasv_mode)
+        #self.cmd.cmd_window.mkdir_pushButton.clicked.connect(self.mkdir)
+        #self.cmd.cmd_window.rmdir_pushButton.clicked.connect(self.rmdir)
         self.window.cmd_window.radioButton_port.clicked.connect(self.port_mode)
         self.window.cmd_window.radioButton_pasv.clicked.connect(self.pasv_mode)
-        self.window.cmd_window.pushButton_connect.clicked.connect(self.new_connection)
+        #self.window.cmd_window.pushButton_connect.clicked.connect(self.new_connection)
         self.local_path = os.getcwd()
         self.window.cmd_window.lineEdit_local_site.setText(self.local_path)
         self.window.cmd_window.lineEdit_remote_site.setText('/tmp')
@@ -64,6 +64,23 @@ class FTPClient(QMainWindow):
         self.window.cmd_window.treeWidget_remote_site.setColumnCount(4)
         self.window.cmd_window.treeWidget_remote_site.setHeaderLabels(['Last modified', 'Type', 'Size', 'Name'])
         self.show_local_dir()
+        self.window.cmd_window.pushButton_local_MKDIR.clicked.connect(self.handle_local_dialog)
+        self.window.cmd_window.pushButton_local_upload.clicked.connect(self.handle_local_dialog)
+        self.window.cmd_window.pushButton_local_rename.clicked.connect(self.handle_local_dialog)
+        self.window.cmd_window.pushButton_local_del.clicked.connect(self.handle_local_dialog)
+        self.window.cmd_window.pushButton_remote_MKDIR.clicked.connect(self.handle_remote_dialog)
+        self.window.cmd_window.pushButton_remote_download.clicked.connect(self.handle_remote_dialog)
+        self.window.cmd_window.pushButton_remote_rename.clicked.connect(self.handle_remote_dialog)
+        self.window.cmd_window.pushButton_remote_del.clicked.connect(self.handle_remote_dialog)
+        self.remote_dir = {}
+        self.window.cmd_window.progressBar_upload.setFormat("%v")
+        self.window.cmd_window.progressBar_upload.reset()
+        self.window.cmd_window.progressBar_download.reset()
+        self.window.cmd_window.progressBar_download.setFormat("%v")
+
+        l = QLabel('test')
+        self.window.cmd_window.tab_finish.layout.addWidget(l)
+
 
     def Login(self):
         try:
@@ -97,6 +114,8 @@ class FTPClient(QMainWindow):
         self.window.cmd_window.lineEdit_port.setText('21')
         self.window.cmd_window.radioButton_pasv.setChecked(True)
         self.window.show()
+        #self.client.pasv()
+        self.show_remote_dir()
         """
         TEST
         """
@@ -133,7 +152,7 @@ class FTPClient(QMainWindow):
                 QMessageBox.information(self, "connection", "连接成功")
             else:
                 QMessageBox.information(self, "connection", "连接失败")
-        self.show_remote_dir()
+        #self.show_remote_dir()
 
     def show_local_dir(self):
         path = self.window.cmd_window.lineEdit_local_site.text()
@@ -160,32 +179,155 @@ class FTPClient(QMainWindow):
 
 
     def show_remote_dir(self, dir=None):
+        if self.client.mode == 'PASV':
+            self.client.pasv()
+        else:
+            self.client.port()
         path = self.window.cmd_window.lineEdit_remote_site.text()
         self.window.cmd_window.treeWidget_remote_site.clear()
         if path[0:4] == '/tmp':
             path = path[4:]
         file_list = self.client.list(path)
         #print(f'file_list: {file_list}')
-        if file_list is None:
-            self.cmd.cmd_window.PASV_radioButton.setChecked(True)
-            self.client.pasv()
-            file_list = self.client.list(None)
+        #if file_list is None:
+        #    self.cmd.cmd_window.PASV_radioButton.setChecked(True)
+        #    self.client.pasv()
+        #    file_list = self.client.list(None)
         file_list = file_list.split('\n')[:-1]
         for file in file_list:
             file = file.split()
             if file[4] == '0':
                 item = QTreeWidgetItem([file[5][:2] + '/' + file[6] + ' ' + file[7], 'DIR', '/', file[-1]])
                 self.window.cmd_window.treeWidget_remote_site.addTopLevelItem(item)
+                self.remote_dir[file[-1]] = 0
             else:
                 item = QTreeWidgetItem([file[5][:2] + '/' + file[6] + ' ' + file[7], 'file', file[4], file[-1]])
                 self.window.cmd_window.treeWidget_remote_site.addTopLevelItem(item)
+                self.remote_dir[file[-1]] = file[4]
 
 
-    def mkdir(self):
-        pass
+    def handle_local_dialog(self):
+        sender = self.sender()
+        if sender == self.window.cmd_window.pushButton_local_MKDIR:
+            dir_name, ok = QInputDialog.getText(self, '新建目录', '请输入目录名：')
+            if ok:
+                self.mkdir_local(dir_name)
 
-    def rmdir(self):
-        pass
+        elif sender == self.window.cmd_window.pushButton_local_upload:
+            filename, ok = QInputDialog.getText(self, '上传文件', '请输入文件名：')
+            if ok:
+                self.upload_local(filename)
+
+        elif sender == self.window.cmd_window.pushButton_local_rename:
+            filename, ok = QInputDialog.getText(self, '重命名文件', '请输入文件名')
+            if ok:
+                new_name, ok = QInputDialog.getText(self, '新文件名', '请输入新文件名')
+                if ok:
+                    self.rename_local(filename, new_name)
+
+        elif sender == self.window.cmd_window.pushButton_local_del:
+            filename, ok = QInputDialog.getText(self, '删除文件', '请输入文件名')
+            if ok:
+                self.rmdir_local(filename)
+
+    def mkdir_local(self, dir_name):
+        if os.path.exists(dir_name):
+            pass
+        os.makedirs(dir_name)
+        self.show_local_dir()
+
+    def rmdir_local(self, filename):
+        if not os.path.exists(filename):
+            pass
+        try:
+            os.remove(filename)
+            self.show_local_dir()
+        except:
+            try:
+                os.removedirs(filename)
+                self.show_local_dir()
+            except:
+                QMessageBox.information(None, 'Error', 'local Del', QMessageBox.Yes)
+                return
+
+    def upload_local(self, filename):
+        if self.client.mode == 'PASV':
+            self.client.pasv()
+        if self.client.mode == 'PORT':
+            self.client.port()
+        self.client.stor('filename', 'filename', self.window.cmd_window.progressBar_upload, 0)
+        self.show_remote_dir()
+
+    def rename_local(self, filename, new_name):
+        if not os.path.exists(filename):
+            pass
+        os.rename(filename, new_name)
+        self.show_local_dir()
+
+    def handle_remote_dialog(self):
+        sender = self.sender()
+        if sender == self.window.cmd_window.pushButton_remote_MKDIR:
+            dir_name, ok = QInputDialog.getText(self, '新建目录', '请输入目录名：')
+            if ok:
+                self.mkdir_remote(dir_name)
+
+        elif sender == self.window.cmd_window.pushButton_remote_download:
+            filename, ok = QInputDialog.getText(self, '下载文件', '请输入文件名：')
+            if ok:
+                self.download_remote(filename)
+
+        elif sender == self.window.cmd_window.pushButton_remote_rename:
+            filename, ok = QInputDialog.getText(self, '重命名文件', '请输入文件名')
+            if ok:
+                new_name, ok = QInputDialog.getText(self, '新文件名', '请输入新文件名')
+                if ok:
+                    self.rename_remote(filename, new_name)
+
+        elif sender == self.window.cmd_window.pushButton_remote_del:
+            filename, ok = QInputDialog.getText(self, '删除文件', '请输入文件名')
+            if ok:
+                self.rmdir_remote(filename)
+
+    def mkdir_remote(self, dir_name):
+        self.client.mkd(dir_name)
+        self.show_remote_dir()
+        self.remote_dir[dir_name] = 0
+
+    def rmdir_remote(self, file_name):
+        self.client.rmd(file_name)
+        self.show_remote_dir()
+        try:
+            del(self.remote_dir[file_name])
+        except:
+            pass
+
+    def rename_remote(self, filename, new_name):
+        self.client.rnfr(filename)
+        self.client.rnto(new_name)
+        self.show_remote_dir()
+        try:
+            size = self.remote_dir[filename]
+            del(self.remote_dir[filename])
+            selt.remote_dir[new_name] = size
+        except:
+            pass
+
+    def download_remote(self, filename):
+        if self.client.mode == 'PASV':
+            print(self.client.pasv())
+        if self.client.mode == 'PORT':
+            print(self.client.port())
+
+        try:
+            size = self.remote_dir[filename]
+            if int(size) == 0:
+                return
+        except:
+            size = 0
+            return
+        print(f'size: {size}')
+        self.client.retr(filename, filename, int(size), 0, self.window.cmd_window.progressBar_download)
+        self.show_local_dir()
 
     def display_list(self, dir=None):
         root = self.cmd.cmd_window.file_list_treeWidget
